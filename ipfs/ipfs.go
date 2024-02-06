cd4209fcdc53c601702cfa9613e94e3295a17471
KelvinWu <kelvinwu@beNovelty.com>
2024-02-06 15:14:35 +0800

    Refactored groupInit

--- ipfs/ipfs.go
+++ ipfs/ipfs.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"log"
 	"net"
-	"net/http"
 	"strings"
 	"sync"
 	"time"
@@ -47,135 +46,117 @@ type discoverProgressProfile struct {
 
 // IPFS implements blueprint.ImmutableStorage.
 type IPFS struct {
-	daemon              *ipfsClient          // daemon object is used to talk to the IPFS daemon.
-	nodeDiscoveryClient *nodeDiscoveryClient // used to talk to the localhost NodeDiscovery service
-	clusterServer       *ClusterServer
-	grpcServer          *grpc.Server
+	ipfsClient          *ipfsClient                  // used to talk to the IPFS daemon.
+	nodeDiscoveryClient *nodeDiscoveryClient         // used to talk to the localhost NodeDiscovery service
+	clusterServer       *grpc.Server                 // gRPC server serving other ImmutableStorage-IPFS nodes
 	keyToCid            map[blueprint.Key]cidProfile // keyToCid stores
 	nodesIPNS           string
 	mappingsIPNS        string
 	discoverProgress    map[string]discoverProgressProfile
 }
 
-func New(configFile string) (*IPFS, error) {
-	// TODO: starting the workers
-	e := errors.New("failed to initialize immutable-storage")
-
-	config, err := parseConfigFile(configFile)
-	if err != nil {
-		log.Println("Error during parseConfigFile...")
-		log.Println(err)
-		return nil, e
-	}
-	//send HEAD request to <host>/api/v0/id
-	err = checkDaemonAlive(config.Host)
-	if err != nil {
-		log.Println("Error during checkDaemonAlive...")
-		log.Println(err)
-		return nil, e
-	}
-	//create new IPFS struct
+func New(configFilePath string) (*IPFS, error) {
+	// Use default config on any error
+	// config := loadConfig(configFilePath)
+	// Create new IPFS struct
 	var ipfs IPFS
-	//set up ipfs daemon
-	ipfs.daemon = newIPFSClient(config.Host, time.Duration(config.Timeout))
-	//initialize node state (lonely init + group init)
-	//lonely init
-	mappingsIPNS, oldNodesIPNS, err := lonelyInitialization(ipfs.daemon)
-	if err != nil {
-		log.Println("Error during lonelyInitialization...")
+	// Connect to all dependencies
+	ipfs.initDependencies()
+
+	// Lonely Initialization. On error retry every 10s.
+	mappingsIPNS, localNodesIPNS, err := ipfs.lonelyInit()
+	for err != nil {
+		log.Println("Error during lonelyInit. Retry after 10s.")
 		log.Println(err)
-		return nil, e
+		time.Sleep(10 * time.Second)
+		mappingsIPNS, localNodesIPNS, err = ipfs.lonelyInit()
 	}
 	ipfs.mappingsIPNS = mappingsIPNS
 
-	//group init
-	newNodesIPNS, err := groupInitialization(&ipfs, oldNodesIPNS, mappingsIPNS, time.Duration(config.Timeout))
+	// Group Initialization. On error ignore and proceed.
+	newNodesIPNS, err := ipfs.groupInit(localNodesIPNS, mappingsIPNS)
 	if err != nil {
-		log.Println("Error during groupInitialization...")
-		log.Println(err)
-		return nil, e
+		log.Println("Error during groupInit. groupInit has no effect.")
 	}
 	ipfs.nodesIPNS = newNodesIPNS
 
-	// start grpc server
-	// TODO: Check if this work...
-	ipfs.grpcServer = grpc.NewServer()
-	ipfs.clusterServer = NewClusterServer(&ipfs)
-	protos.RegisterImmutableStorageClusterServer(ipfs.grpcServer, *ipfs.clusterServer)
-	reflection.Register(ipfs.grpcServer)
+	// Create gRPC server serving other ImmutableStorage-IPFS nodes. Blocking.
+	ipfs.initClusterServer()
 
-	l, err := net.Listen("tcp", "127.0.0.1:3101")
-	if err != nil {
-		log.Println("failed to create gRPC server on port 3101")
-		log.Println(err)
-		return nil, e
-	}
-	if err := ipfs.grpcServer.Serve(l); err != nil {
-		log.Println("failed to create gRPC server on port 3101")
-		log.Println(err)
-		return nil, e
-	}
-	log.Println("Successfully created gRPC server on port 3101")
-	// start worker processes
 	// TODO: create worker functions
 
 	return &ipfs, nil
 }
 
-func checkDaemonAlive(host string) error {
-	cli := &http.Client{}
-	res, err := cli.Head(host + "/api/v0/id")
-	if err != nil {
-		log.Println(err)
-		return errors.New("failed to send health check request")
-	}
-	switch res.StatusCode {
-	case 405:
-		return nil
-	default:
-		return errors.New("abnormal response from daemon")
-	}
+// Load config file if specified. On error, proceed with default config.
+func loadConfig(configFilePath string) config {
+	// Open the file
+	file, _ := openFileWithPath(configFilePath)
+	// Parse the file
+	config, _ := parseConfig(file)
+	return config
 }
 
-// lonelyInitialization returns mappingsIPNS, nodesIPNS, err
-func lonelyInitialization(daemon *ipfsClient) (string, string, error) {
+// initDependencies creates connections with the dependencies of ImmutableStorage-IPFS, including
+// 1) NodeDiscovery grpc server:  localhost:3200
+// 2) IPFS daemon http server  :  localhost:5001
+//
+// This is a blocking procedure and it will retry infinitely on error
+// TODO: add support for configurable ports
+func (ipfs *IPFS) initDependencies() {
+	// Establish connection with IPFS daemon
+	ipfs.ipfsClient = newIPFSClient("localhost:5001", 10*time.Second)
+	log.Println("Connected to localhost:5001 -- IPFS daemon")
+	// Establish connection with NodeDiscovery grpc server
+	var err error
+	ipfs.nodeDiscoveryClient, err = newNodeDiscoveryClient("localhost:3200", 3*time.Second)
+	for err != nil {
+		// On error, retry every 1s
+		time.Sleep(time.Second)
+		ipfs.nodeDiscoveryClient, err = newNodeDiscoveryClient("localhost:3200", 3*time.Second)
+	}
+	log.Println("Connected to localhost:3200 -- NodeDiscovery")
+}
+
+// lonelyInit returns mappingsIPNS, nodesIPNS, err
+func (ipfs *IPFS) lonelyInit() (string, string, error) {
 	// TODO: also consider the case when existing IPNS are provided for mapping IPNS and nodesIPNS
 	// initialize public keys: nodes, mappings
 	log.Println("Started lonely initialization...")
-	keyNames, err := daemon.listKeys()
+	keyNames, err := ipfs.ipfsClient.listKeys()
 	if err != nil {
 		return "", "", err
 	}
 	if !slices.Contains(keyNames, "mappings") {
-		if err = daemon.generateKey("mappings"); err != nil {
+		if err = ipfs.ipfsClient.generateKey("mappings"); err != nil {
 			return "", "", err
 		}
 	}
 	if !slices.Contains(keyNames, "nodes") {
-		if err = daemon.generateKey("nodes"); err != nil {
+		if err = ipfs.ipfsClient.generateKey("nodes"); err != nil {
 			return "", "", err
 		}
 	}
 	log.Println("Successfully created keys")
 	// create MFS directory /mappings, ignore error for file already exists
-	if err = daemon.createDirectory("/mappings"); err != nil && err.Error() != "files/mkdir: file already exists" {
+	if err = ipfs.ipfsClient.createDirectory("/mappings"); err != nil && err.Error() != "files/mkdir: file already exists" {
 		return "", "", err
 	}
 	// publish IPNS name for directory /mappings
-	mappingsCID, err := daemon.getDirectoryCID("/mappings")
+	mappingsCID, err := ipfs.ipfsClient.getDirectoryCID("/mappings")
 	if err != nil {
 		return "", "", err
 	}
-	mappingsIPNS, err := daemon.publishIPNSPointer(mappingsCID, "mappings")
+	mappingsIPNS, err := ipfs.ipfsClient.publishIPNSPointer(mappingsCID, "mappings")
 	if err != nil {
 		return "", "", err
 	}
 	log.Println("Successfully published mappingsIPNS:", mappingsIPNS)
 	// create and append mappingIPNS in file /nodes.txt with comma delimiter
-	_, err = daemon.getDirectoryCID("/nodes.txt")
+	_, err = ipfs.ipfsClient.getDirectoryCID("/nodes.txt")
 	if err == nil {
 		// if file exists, remove it
-		if err2 := daemon.removeFile("/nodes.txt", false); err2 != nil {
+		if err2 := ipfs.ipfsClient.removeFile("/nodes.txt", false); err2 != nil {
 			return "", "", err2
 		}
 	} else if err.Error() != "files/stat: file does not exist" {
@@ -184,15 +165,15 @@ func lonelyInitialization(daemon *ipfsClient) (string, string, error) {
 	}
 	log.Println("Successfully cleaned old /nodes.txt")
 
-	if err = daemon.appendStringToFile("/nodes.txt", mappingsIPNS+";", nodestxtMaxSize); err != nil {
+	if err = ipfs.ipfsClient.appendStringToFile("/nodes.txt", mappingsIPNS+";", nodestxtMaxSize); err != nil {
 		return "", "", err
 	}
 	// publish IPNS name for file /nodes.txt
-	nodesCID, err := daemon.getDirectoryCID("/nodes.txt")
+	nodesCID, err := ipfs.ipfsClient.getDirectoryCID("/nodes.txt")
 	if err != nil {
 		return "", "", err
 	}
-	nodesIPNS, err := daemon.publishIPNSPointer(nodesCID, "nodes")
+	nodesIPNS, err := ipfs.ipfsClient.publishIPNSPointer(nodesCID, "nodes")
 	if err != nil {
 		return "", "", err
 	}
@@ -200,41 +181,104 @@ func lonelyInitialization(daemon *ipfsClient) (string, string, error) {
 	return nodesIPNS, mappingsIPNS, nil
 }
 
-// groupInitialization returns nodesIPNS, err
-func groupInitialization(ipfs *IPFS, myNodesIPNS string, myMappingsIPNS string, timeout time.Duration) (string, error) {
-	// TODO: error handlings
-
-	log.Println("Start group initialization...")
+// groupInit is the second phase of node initialization. It is an optional phase and it's absence not affect the algorithm correctness.
+//
+// It returns the most updated IPNS name pointing at nodes.txt. Returns existing IPNS name for nodes.txt on error.
+//
+// It performs the following actions:
+// 1) advertise my own mappingsIPNS to other nodes, allow them to append to their nodes.txt
+// 2) query 3 other nodes and aggregate their nodes.txt
+// 3) append the aggregated results to my own nodes.txt
+// 4) update the IPNS pointer to point at the latest nodes.txt
+//
+// Error semantics: return errors if this function fails before Step 3, which could be safely ignored as this function has not yet mutate any states.
+// This function will block the goroutine if it fails at or after Step 3, and keep retrying until success.
+func (ipfs *IPFS) groupInit(myNodesIPNS string, myMappingsIPNS string) (string, error) {
 	// On error retry every 10s, blocking
 	memberIPs, err := ipfs.nodeDiscoveryClient.getNMembers(3)
 	if err != nil {
+		log.Println("[groupInit]:Failed call nodeDiscoveryClient.getNMembers. Skip groupInit.")
+		log.Println(err)
 		return myNodesIPNS, err
 	}
+	log.Println("[groupInit]:Success call nodeDiscoveryClient.getNMembers")
 	// On error no retry, non-blocking
 	propagateWriteExternal(myMappingsIPNS, memberIPs)
-	log.Println("Successfully propagated write")
+	log.Println("[groupInit]:Success call propagateWriteExternal")
 
 	// On error retry every 10s, blocking
 	validatedMappingsIPNSs, err := ipfs.initNodestxt()
 	if err != nil {
+		log.Println("[groupInit]:Failed call ipfs.initNodestxt. Skip groupInit.")
+		log.Println(err)
 		return myNodesIPNS, err
 	}
-	log.Println("Successfully validated all mappingsIPNSs: to be appended =", len(validatedMappingsIPNSs))
+	log.Println("[groupInit]:Success call ipfs.initNodestxt")
 
 	// Append all mappingsIPNS to my own nodes.txt
 	// See parser.go for expected format of nodes.txt. Append empty string at the end so that last element also has ending ';' after Join.
 	formattedContent := strings.Join(append(validatedMappingsIPNSs, ""), ";")
 	// On error retry 3 times, blocking
-	ipfs.daemon.appendStringToFile("/nodes.txt", formattedContent, nodestxtMaxSize)
+	err = ipfs.ipfsClient.appendStringToFile("/nodes.txt", formattedContent, nodestxtMaxSize)
+	for retryCount, maxRetry := 0, 3; err != nil && retryCount < maxRetry; retryCount++ {
+		log.Println("[groupInit]:Failed call ipfsClient.appendStringToFile. Retry = (", retryCount, "/", maxRetry, ")")
+		log.Println(err)
+		err = ipfs.ipfsClient.appendStringToFile("/nodes.txt", formattedContent, nodestxtMaxSize)
+	}
+	if err != nil {
+		log.Println("[groupInit]:Failed call ipfsClient.appendStringToFile. Skip groupInit.")
+		log.Println(err)
+		return myNodesIPNS, err
+	}
+	log.Println("[groupInit]:Success call ipfsClient.appendStringToFile")
 
 	// On error retry every 10s, blocking
-	nodesCID, err := ipfs.daemon.getDirectoryCID("/nodes.txt")
-	if err != nil {
-		return "", err
+	nodesCID, err := ipfs.ipfsClient.getDirectoryCID("/nodes.txt")
+	for err != nil {
+		log.Println("[groupInit]:Failed call ipfsClient.getDirectoryCID. Retry after 10s.")
+		time.Sleep(10 * time.Second)
+		nodesCID, err = ipfs.ipfsClient.getDirectoryCID("/nodes.txt")
 	}
+	log.Println("[groupInit]:Success call ipfsClient.getDirectoryCID")
+
 	// On error retry every 10s, blocking
-	nodesIPNS, err := ipfs.daemon.publishIPNSPointer(nodesCID, "nodes")
+	nodesIPNS, err := ipfs.ipfsClient.publishIPNSPointer(nodesCID, "nodes")
+	for err != nil {
+		log.Println("[groupInit]:Failed call ipfsClient.publishIPNSPointer. Retry after 10s.")
+		time.Sleep(10 * time.Second)
+		nodesIPNS, err = ipfs.ipfsClient.publishIPNSPointer(nodesCID, "nodes")
+	}
+	log.Println("[groupInit]:Success call ipfsClient.publishIPNSPointer")
+
 	return nodesIPNS, err
+}
+
+// Create a gRPC server to serve requests from other ImmutableStorage-IPFS nodes.
+// Blocking.
+func (ipfs *IPFS) initClusterServer() {
+	ipfs.clusterServer = grpc.NewServer()
+	protos.RegisterImmutableStorageClusterServer(ipfs.clusterServer, NewClusterServer(ipfs))
+	reflection.Register(ipfs.clusterServer)
+
+	// On error retry every 5s. Blocking.
+	listener, err := net.Listen("tcp", "127.0.0.1:3101")
+	for err != nil {
+		log.Println("[initClusterServer]:Failed call net.Listen. Retry after 5s.")
+		log.Println(err)
+		time.Sleep(5 * time.Second)
+		listener, err = net.Listen("tcp", "127.0.0.1:3101")
+	}
+	log.Println("[initClusterServer]:Success call net.Listen")
+
+	// On error retry every 5s. Blocking.
+	err = ipfs.clusterServer.Serve(listener)
+	for err != nil {
+		log.Println("[initClusterServer]:Failed call grpcServer.Serve. Retry after 5s.")
+		log.Println(err)
+		time.Sleep(5 * time.Second)
+		err = ipfs.clusterServer.Serve(listener)
+	}
+	log.Println("[initClusterServer]:Success call grpcServer.Serve")
 }
 
 func (ipfs *IPFS) Store(key blueprint.Key, message []byte) error {
@@ -243,14 +287,14 @@ func (ipfs *IPFS) Store(key blueprint.Key, message []byte) error {
 		return errors.New("invalid key")
 	}
 	// store the message on ipfs
-	cid, err := ipfs.daemon.addFile(message)
+	cid, err := ipfs.ipfsClient.addFile(message)
 	if err != nil {
 		return err
 	}
 	// append the cid in the current mapping file
 	// TODO handle offset, next page logic
 	keyStr := string(key[:])
-	ipfs.daemon.appendStringToFile("/mappings/???...", keyStr+","+cid+";", mappingPageMaxSize)
+	ipfs.ipfsClient.appendStringToFile("/mappings/???...", keyStr+","+cid+";", mappingPageMaxSize)
 
 	// TODO propagate write
 	// var peers []string = []string{"1.1.1.1", "2.2.2.2", "3.3.3.3"}
@@ -292,18 +336,18 @@ func (ipfs *IPFS) IsDiscovered(key blueprint.Key) bool {
 	return discovered
 }
 
+// This function aims to speed up the latency for a remote node to discover a new local message stored on IPFS.
+// Elimination of this function should have no impact on the correctness of the algorithm.
+//
+// It performs the following actions:
+// 1) trigger the update IPNS routine, sync the local state regarding this IPNS with remote IPFS state.
+// 2) proceed if actual update is performed
+// 3) call NodeDiscovery.GetNMembers(fanout) to retrieve any N random members ip.
+// 4) call propagateWrite on each members in non-blocking manner
+//
+// Regarding step 4, non-blocking manner means that this function may return before it receives propagateWrite responses from the other members.
+// In other words, this function does not guarantee it will propagate the updatedMappingsIPNS to any node.
 func (ipfs *IPFS) propagateWriteInternal(updatedMappingsIPNS string) error {
-	// This function aims to speed up the latency for a remote node to discover a new local message stored on IPFS.
-	// Elimination of this function should have no impact on the correctness of the algorithm.
-	//
-	// It performs the following actions:
-	// 1) trigger the update IPNS routine, sync the local state regarding this IPNS with remote IPFS state.
-	// 2) proceed if actual update is performed
-	// 3) call NodeDiscovery.GetNMembers(fanout) to retrieve any N random members ip.
-	// 4) call propagateWrite on each members in non-blocking manner
-	//
-	// Regarding step 4, non-blocking manner means that this function may return before it receives propagateWrite responses from the other members.
-	// In other words, this function does not guarantee it will propagate the updatedMappingsIPNS to any node.
 
 	// TODO: Step 1, 2
 
@@ -340,17 +384,17 @@ func propagateWriteExternal(updatedMappingsIPNS string, memberIPs []string) {
 	}
 }
 
+// This function allow a fresh Immutable Storage node instance to initialize its nodes.txt based on the current cluster state.
+// returns a slice of validated mappingsIPNS learnt from cluster peers.
+// returns nil on error
+//
+// It performs the following actions:
+// 1) query n nodes in the network to obtain their nodesIPNS
+// 2) resolve each nodesIPNS's contents as a slice of string, each element is a mappingIPNS
+// 3) remove duplicates among all n slices of string, combine them into one slice, and return.
+//
+// Error handling: error of any particular query node will be ignored.
 func (ipfs IPFS) initNodestxt() ([]string, error) {
-	// This function allow a fresh Immutable Storage node instance to initialize its nodes.txt based on the current cluster state.
-	// returns a slice of validated mappingsIPNS learnt from cluster peers.
-	// returns nil on error
-	//
-	// It performs the following actions:
-	// 1) query n nodes in the network to obtain their nodesIPNS
-	// 2) resolve each nodesIPNS's contents as a slice of string, each element is a mappingIPNS
-	// 3) remove duplicates among all n slices of string, combine them into one slice, and return.
-	//
-	// Error handling: error of any particular query node will be ignored.
 
 	queryTargets, err := ipfs.nodeDiscoveryClient.getNMembers(3)
 	if err != nil {
@@ -379,7 +423,7 @@ func (ipfs IPFS) initNodestxt() ([]string, error) {
 				return
 			}
 
-			externalNodestxt, err := openFileWithIPNS(ipfs.daemon, nodesIPNS)
+			externalNodestxt, err := openFileWithIPNS(ipfs.ipfsClient, nodesIPNS)
 			if err != nil {
 				log.Println("[GetNodestxt]:Failed to open", externalNodestxt, "from", addr, ". error:", err)
 				return
@@ -411,7 +455,7 @@ func (ipfs IPFS) initNodestxt() ([]string, error) {
 		wg.Add(1)
 		go func() {
 			// Ignore the mappingsIPNS which causes error during validation
-			ok, _ := ipfs.daemon.validateMappingsIPNS(mappingsIPNS, 10)
+			ok, _ := ipfs.ipfsClient.validateMappingsIPNS(mappingsIPNS, 10)
 			// This may cause the hashset.Range method to revisit this key, but it is ok as this goroutine is idempotent
 			hashset.Store(mappingsIPNS, ok)
 			wg.Done()
@@ -435,8 +479,8 @@ func (ipfs IPFS) initNodestxt() ([]string, error) {
 	return validatedMappingsIPNSs, nil
 }
 
+// return the corresponding CID and the IPNS name of the key if discovered
 func (ipfs IPFS) sync(key blueprint.Key) (string, string) {
-	// return the corresponding CID and the IPNS name of the key if discovered
 	if ipfs.IsDiscovered(key) {
 		cidProfile := ipfs.keyToCid[key]
 		return cidProfile.cid, cidProfile.source
