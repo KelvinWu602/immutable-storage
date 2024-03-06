48160816853d68e4a2da634fb7374fb9c8cdec87
KelvinWu602 <wuhfhf@gmail.com>
2024-03-06 12:53:55 +0800

    Edited common.go DEBUG Logs

--- blueprint/common.go
+++ blueprint/common.go
@@ -10,6 +10,7 @@ import (
 // key: a 48 byte key that will be used to associate with the message
 // message: an array of bytes begins with the key, followed by message content
 func ValidateKey(key Key, message []byte) bool {
+	// log.Println("DEBUG: called validate key")
 	msgCopy := make([]byte, len(message))
 	copy(msgCopy, message)
 	// check message header = key
@@ -22,5 +23,13 @@ func ValidateKey(key Key, message []byte) bool {
 	msgContent := msgCopy[KeySize:]
 	correctChecksum := sha256.Sum256(append(msgUUID, msgContent...))
 	keyChecksum := key[len(key)-32:]
+
+	debug := false
+	if debug {
+		log.Println("msgUUID      :", msgUUID)
+		log.Println("msgContent   :", msgContent)
+		log.Println("correctChecksum:", correctChecksum)
+		log.Println("keyChecksum    :", keyChecksum)
+	}
 	return bytes.Equal(correctChecksum[:], keyChecksum)
 }
